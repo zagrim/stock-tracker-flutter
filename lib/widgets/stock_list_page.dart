d29ac1d920aaea37a5fe3bb745c1a8f64caa11fe
Esa Tikka <esa.tikka@iki.fi>
2022-11-13 11:10:11 +0200

    Some clean-up, tweaks

--- lib/widgets/stock_list_page.dart
+++ lib/widgets/stock_list_page.dart
@@ -17,7 +17,6 @@ class _StockListPageState extends State<StockListPage> {
     Stock('QTCOM', 'Qt Group Oyj', 'EUR', DateTime.now(), 135.60, -0.008),
     Stock('MUSTI', 'Musti Group Oyj', 'EUR', DateTime.now(), 30.43, 0.0032),
   ];
-  late double _appBarHeight;
   var _showAddForm = false;
 
   @override
@@ -30,14 +29,18 @@ class _StockListPageState extends State<StockListPage> {
             setState(() {
               _showAddForm = !_showAddForm;
             });
-          }, //_onShowModal(context),
+          },
           icon: const Icon(Icons.add),
         ),
       ],
     );
-    _appBarHeight = _appBar.preferredSize.height;
+    final _appBarHeight = _appBar.preferredSize.height;
+    final _mediaQuery = MediaQuery.of(context);
+    final _contentMaxHeight =
+        _mediaQuery.size.height - _appBarHeight - _mediaQuery.padding.vertical;
 
-    return Scaffold(
+    return SafeArea(
+        child: Scaffold(
       appBar: _appBar,
       body: SingleChildScrollView(
         child: Column(
@@ -45,31 +48,27 @@ class _StockListPageState extends State<StockListPage> {
           children: [
             if (_showAddForm)
               SizedBox(
-                height: MediaQuery.of(context).size.height -
-                    _appBarHeight -
-                    MediaQuery.of(context).padding.vertical,
+                height: _contentMaxHeight,
                 child: AddStockForm(_onAddStock),
               ),
             if (!_showAddForm)
               SizedBox(
-                height: MediaQuery.of(context).size.height -
-                    _appBarHeight -
-                    MediaQuery.of(context).padding.vertical,
+                height: _contentMaxHeight,
                 child: StockList(stockList: _stocks),
               ),
             /*LayoutBuilder(builder: (context, constraints) {
-              return SizedBox(
-                height: constraints.maxHeight,
-                child: StockList(stockList: _stocks),
-              );
-            })*/
+                  return SizedBox(
+                    height: constraints.maxHeight,
+                    child: StockList(stockList: _stocks),
+                  );
+                })*/
             /* BoxConstraints forces an infinite height.
-            These invalid constraints were provided to RenderConstrainedBox's layout() function by the following function, which probably computed the invalid constraints in question:
-            RenderConstrainedBox.performLayout*/
+                These invalid constraints were provided to RenderConstrainedBox's layout() function by the following function, which probably computed the invalid constraints in question:
+                RenderConstrainedBox.performLayout*/
           ],
         ),
       ),
-    );
+    ));
   }
 
   void _onAddStock(Stock stock) {
